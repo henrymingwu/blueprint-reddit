ecbff225944261015f79fd0d5042a53954f5762f
Deepak Warrier <dwarrier@gmail.com>
2014-02-06 20:48:38 -0800

    added vars to Article class

--- Article.java
+++ Article.java
@@ -2,13 +2,57 @@ import java.util.*;
 
 public class Article {
 
-int favorites;
-User user;
-ArrayList<Community> communities;
-ArrayList<String> keywords;
-String title;
-String content;
+private int favorites;
+private User user;
+private ArrayList<Community> communities;
+private ArrayList<String> keywords;
+private String title;
+private String content;
 
-public int getFavorites {
-  
+public int getFavorites() {
+  return this.favorites;  
+}
+
+public User getUser() {
+  return this.user;  
+}
+
+public ArrayList<Community> getCommunity() {
+  return this.communities;
+}
+
+public ArrayList<String> getKeywords() {
+  return this.keywords;
+}
+
+public String getTitle() {
+  return this.title;
+}
+
+public String getContent() {
+  return this.content;
+}
+
+public int setFavorites(int num) {
+  this.favorites = num; 
+}
+
+public User setUser(User user) {
+  this.user = user;
+}
+
+public ArrayList<Community> setCommunity(Community comm) {
+  this.community = comm;
+}
+
+public ArrayList<String> setKeywords(ArrayList<String> keys) {
+  this.keywords = keys;
+}
+
+public String setTitle(String title) {
+  this.title = title;
+}
+
+public String setContent(String con) {
+  this.content = con;
 }
