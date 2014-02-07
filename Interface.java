7ca0f470453e8280a8aa25bb7ab9a775c18c2471
Henry Wu <henry.wu@berkeley.edu>
2014-02-06 21:39:19 -0800

    merged with deepak again

--- Interface.java
+++ Interface.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class Interface
 {
+  public int articleCounter;
+  public HashMap<String,User> usersByName;
+  public HashMap<String,Article> articlesByName;
+  public HashMap<String,Community> communitiesByName;
+  public ArrayList<Article> allArticles;
+  public ArrayList<User> allUsers;
+  public ArrayList<Community> allCommunities;
+   
+    public Interface() {
+      articleCounter = 0;
+      usersByName = new HashMap<String,User>();
+      articlesByName = new HashMap<String,Article>();
+      communitiesByName = new HashMap<String,Community>();
+      allArticles = new ArrayList<Article>();
+      allUsers = ArrayList<User>(); 
+      allCommunities = ArrayList<Community>();
+    } 
 
     public static void main(string[] args)
     {
@@ -33,13 +52,48 @@ public class Interface
         }
 
     }
+    
+    public void printAllUsers() {
+      for (User user : allUsers) {
+        System.out.println(user.getName()) {
+      } 
+    } 
+    
+    public void printAllArticles() {
+      for (Article article: allArticles) {
+        System.out.println(articles.getName()) {
+      } 
+    
+    }
+    
+    public void printAllArticlesFromUser(String username) {
+      User user = 
+      if (user == null) {
+        return;
+      }
+      System.out.print 
+      for (Article : user.articles) {
+        System. 
+    } 
+
+    public void printAllArticlesFromCommunity(String comm) {
+
+    }
+ 
+    public void printArticlesFromPast(String days) {
+
+    }
+
+    public printTopArticles(String num) {
+
+    }
 
     public void addArticle(String author, String title, String content, ArrayList<String> keywords, ArrayList<Community> communities)
     {
 
     }
 
-    public void addUser(String userName)
+    public void addUser(String userName) 
     {
 
     }
@@ -48,7 +102,7 @@ public class Interface
     {
 
     }
-
+    
     private void doUserActions(Console c)
     {
         String nextAction = c.readLine("Commands:\n"
@@ -117,8 +171,12 @@ public class Interface
         else if (nextAction.equals("findCommunity"))
         {
             String community = c.readLine("What community?");
-            
+
             //printComm
+        }
+        else if (nextAction.equals("mostFavorited"))
+        {
+            //doCommunityActions(c);
         }
         else
         {
@@ -129,14 +187,6 @@ public class Interface
     }
 
 }
-
-
-
-
-
-
-
-
 
 
 
