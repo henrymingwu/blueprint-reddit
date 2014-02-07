c73e8843b42ad6f300b8aace8184e8dff43794ab
Deepak Warrier <dwarrier@gmail.com>
2014-02-06 20:40:37 -0800

    first commit for Article

---
+++ Article.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Article {
+
+int favorites;
+User user;
+ArrayList<Community> communities;
+ArrayList<String> keywords;
+String title;
+String content;
+
+public int getFavorites {
+  
+}
